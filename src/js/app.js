1bb83df89dfe64c3bea86a120181807f0765427d
Nick <z.nick00@gmail.com>
2020-07-15 14:37:43 -0400

    Adds smooth-scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import mQ from './functions/mediaQueries';
+import SmoothScroll from 'smooth-scroll';
 
 const hamburgerButton = document.getElementById('hamburger-button');
 const mainMenu = document.getElementById('main-menu');
@@ -10,3 +11,5 @@ hamburgerButton.addEventListener('click', function () {
         mainMenu.classList.toggle('is-active');
     }
 });
+
+const scroll = new SmoothScroll('a[href*="#"]');
